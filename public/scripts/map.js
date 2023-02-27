426ba0d71e9a769169607e7aebf3cdf2ecb3d550
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-02-27 22:22:52 +0000

    Add map marker

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,9 +1,16 @@
 let map;
 
 function initMap() {
+  const vancouver = { lat: 49.2578262, lng: -123.1941156 };
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 49.2578262, lng: -123.1941156 },
+    center: vancouver,
     zoom: 12,
+  });
+  const ubc = { lat: 49.26410715655254, lng: -123.24569741813465};
+  const marker = new google.maps.Marker({
+    position: ubc,
+    map: map,
+    title: 'UBC'
   });
 }
 
--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,8 @@
 const express = require('express');
 const router  = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
   res.render('users');
-}); 
+});
 
 module.exports = router;
