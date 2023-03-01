dbce1eac36afa0783a9e1d148c6e9ff8f4d5dab2
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-02-28 20:40:19 -0800

    Add predictive search box search box for users to enter geographical searches. The search box returns a pick list that includes both places and predicted search terms.

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -9,22 +9,84 @@ function initMap() {
     center: vancouver,
     zoom: 12,
   });
+  // Create the search box and link it to the UI element.
+  const input = document.getElementById("places-search");
+  const searchBox = new google.maps.places.SearchBox(input);
 
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener("bounds_changed", () => {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  let markers = [];
+
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        })
+      );
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+
+
+  // Add event listener to add a pin on user click
   map.addListener("click", (e) => {
     addPin(e.latLng, map);
   });
 
 
-  const marker = new google.maps.Marker({
-    position: ubc,
-    map: map,
-    title: 'UBC'
-  });
+  // const marker = new google.maps.Marker({
+  //   position: ubc,
+  //   map: map,
+  //   title: 'UBC'
+  // });
 
   infoWindow = new google.maps.InfoWindow();
-
+  // create Show Current Location button to geolocate users
   const locationButton = document.createElement("button");
-
   locationButton.textContent = "Show Current Location";
   locationButton.classList.add("custom-map-control-button");
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
@@ -52,6 +114,7 @@ function initMap() {
       handleLocationError(false, infoWindow, map.getCenter());
     }
   });
+
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
