0e4af5f91787e325b7691f8508c27984ca1650f0
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-03-01 15:09:50 -0800

    Add pin_drop_form component

---
+++ public/scripts/components/pin_drop_form.js
@@ -0,0 +1,33 @@
+// this is the form containing data users input about their pin
+
+$(() => {
+
+  const $pinDropForm = $(`
+  <div id="pindrop-form">
+  <form method="POST" action="/api/pins">'
+  '<label for="name">Name:</label><br>' +
+  '<input type="text" id="Name" name="name"><br><br>' +
+  '<label for="pinDescription">What makes it special:</label><br>' +
+  '<input type="text" id="description" name="description"><br><br>' +
+  '</form>' +
+  '<button type="submit" form="pindrop-form" value="Submit">Save Pin</button>' +
+  '<button type="button" form="pindrop-form" value="Submit">Cancel</button>'
+  `);
+
+  window.$pinDropForm = $pinDropForm;
+
+  $pinDropForm.on('submit', function (e) {
+  e.preventDefault();
+
+  const data = $(this).serialize();
+  submitPinInfo(data)
+    .then((json) => {
+      header.update(json.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+  });
+
+});
+
