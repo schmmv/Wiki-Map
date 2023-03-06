043dbe0d28f4bb6be2d7d5a26e35ae9d8d0119af
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-03-06 00:17:15 +0000

    Move getFavsByUserId to favourites queries

--- db/queries/maps.js
+++ db/queries/maps.js
@@ -17,17 +17,7 @@ const getMapById = function(id) {
     });
 };
 
-const getFavsByUserId = function(id) {
-  return db.query(`
-    SELECT maps.title, maps.user_id, latitude, longitude, zoom
-    FROM maps
-    JOIN favourites ON maps.id = map_id
-    JOIN users ON users.id = favourites.user_id
-    WHERE favourites.user_id = $1`, [id])
-    .then(data => {
-      return data.rows;
-    });
-}
+
 
 const getMapsByUserId = function(id) {
   return db.query(`
@@ -40,4 +30,4 @@ const getMapsByUserId = function(id) {
     });
 }
 
-module.exports = { getAllMaps, getMapById, getFavsByUserId, getMapsByUserId };
+module.exports = { getAllMaps, getMapById, getMapsByUserId };
