5f263eb2c9c6d6dba589c3f160189da52d1bdf89
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-03-08 03:24:18 +0000

    Add ajax requests based on heart icon click

--- public/scripts/favourite.js
+++ public/scripts/favourite.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
   $('.fa-heart').click(function() {
+    // console.log('before click:', $(this).attr('class'));
+    const $mapId = $(this).attr('id');
+
+    if ($(this).attr('class') === 'fa-regular fa-heart') {
+      // add to favourites
+      $.ajax({
+        method: 'POST',
+        url: '/api/favourites/add',
+        data: {map : $mapId}
+      })
+    } else if ($(this).attr('class') === 'fa-solid fa-heart') {
+      //delete from favourites
+      $.ajax({
+        method: 'POST',
+        url: `/api/favourites/${$mapId}`
+      })
+    }
+
     $(this).toggleClass("fa-solid fa-heart fa-regular fa-heart")
 
   })
