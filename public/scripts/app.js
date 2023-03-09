31029a8fd7aa57b5be120bb8bf0deea10cf019a6
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-03-09 13:33:42 -0800

    Move map variable to global scope

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,11 @@
-
+let map;
 // Initialize and add the map
 function initMapView() {
   console.log(mapData.lat, mapData.lng);
 
     const center = { lat: mapData.lat, lng: mapData.lng };
     // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("map"), {
       zoom: mapData.zoom,
       center
     });
