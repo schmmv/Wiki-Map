c4a1ef35168158ff2f88c860ed33f97d27d712c0
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-03-10 22:47:03 +0000

    Update pin seeds with github img links

--- db/seeds/03_pins.sql
+++ db/seeds/03_pins.sql
@@ -1,10 +1,10 @@
 INSERT INTO pins (user_id, map_id, title, description, image_url, latitude, longitude)
-VALUES (1, 1, 'UBC', 'Ranked top 20 worldwide', 'https://lh5.googleusercontent.com/p/AF1QipNJbVuY_D7H22FUEMltO5qZYyVmf-IwS6aG79eK=w408-h306-k-no', 49.265338222605244, -123.24569339722409),
-(2, 1, 'Vancouver Film School', 'Entertainment arts and media school', 'https://lh3.googleusercontent.com/p/AF1QipMmxoeFCqHM1gho004uijSDwJPbmNl06KSm-bsn=s680-w680-h510', 49.28269679538249, -123.1094589),
-(1, 1, 'SFU', 'Known for science and engineering degrees', 'https://lh5.googleusercontent.com/p/AF1QipMQSj1c7tlKGGTtusvz2W1hhMmQkJVwq9mFitOr=w408-h306-k-no', 49.28493643379976, -122.91906129346351),
-(1, 2, 'Miner''s Path', 'Easy 2km walk to a waterfall', 'https://lh5.googleusercontent.com/p/AF1QipO-0bZXaus2_JvQVR3kRkCKHCX98OtbdRzp73PQ=w408-h544-k-no', 49.63871384026196, -114.50494142050628),
-(1, 2, 'Star Creek Falls', 'Nice short hike along the river ending in a great view of the falls', 'https://lh5.googleusercontent.com/p/AF1QipMtmVflxkUMwLPyTfMl7bME07GhYyif7XFEDIZU=w426-h240-k-no', 49.6274044255323, -114.53859833158945),
-(2, 3, 'Greenbelt Trailhead', 'Easy 4 mile loop trail', 'https://picsum.photos/id/1/203', 50.995512050761604, -118.20266841360453),
+VALUES (1, 1, 'UBC', 'Ranked top 20 worldwide', 'https://github.com/schmmv/Wiki-Map/blob/master/public/images/UBC.jpg?raw=true', 49.265338222605244, -123.24569339722409),
+(2, 1, 'Vancouver Film School', 'Entertainment arts and media school', 'https://github.com/schmmv/Wiki-Map/blob/master/public/images/VFS.jpg?raw=true', 49.28269679538249, -123.1094589),
+(1, 1, 'SFU', 'Known for science and engineering degrees', 'https://github.com/schmmv/Wiki-Map/blob/master/public/images/SFU.jpg?raw=true', 49.28493643379976, -122.91906129346351),
+(1, 2, 'Miner''s Path', 'Easy 2km walk to a waterfall', 'https://github.com/schmmv/Wiki-Map/blob/master/public/images/miners_path.jpg?raw=true', 49.63871384026196, -114.50494142050628),
+(1, 2, 'Star Creek Falls', 'Nice short hike along the river ending in a great view of the falls', 'https://github.com/schmmv/Wiki-Map/blob/master/public/images/star_creek.jpg?raw=true', 49.6274044255323, -114.53859833158945),
+(2, 3, 'Greenbelt Trailhead', 'Easy 4 mile loop trail', 'https://github.com/schmmv/Wiki-Map/blob/master/public/images/greenbelt.jpg?raw=true', 50.995512050761604, -118.20266841360453),
 (2, 4, 'Royal Tyrrell Museum', 'Ultimate dinosaur museum', 'https://lh5.googleusercontent.com/p/AF1QipNza5_QxoDsL9iMedYDXBcygkmr58-YTBzpJBAf=w408-h306-k-no', 51.479685173715005, -112.78995105600498),
-(1, 5, 'Fernie Brewing Company', 'Best beer in town', 'https://lh5.googleusercontent.com/p/AF1QipNlZnPBxaHRVBwloUQBrDZVeU9HTO3gWNRgj8vA=w408-h515-k-no', 49.52681206007952, -115.0476444279735),
+(1, 5, 'Fernie Brewing Company', 'Best beer in town', 'https://github.com/schmmv/Wiki-Map/blob/master/public/images/fernie_brewing.jpg?raw=true', 49.52681206007952, -115.0476444279735),
 (2, 5, 'Oldman River Brewing', 'The one and only - don''t miss the Ephemerella hopped sour!', 'https://lh5.googleusercontent.com/p/AF1QipPiXjtVgDJOVWanYS0dtu9PK8uTGGDMFydU74fb=w408-h408-k-no',49.58717747698649, -114.1623618865719);
