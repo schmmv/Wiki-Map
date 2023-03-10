797e97fec3b661302021a906966291611348db74
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-03-09 17:10:51 -0800

    Deleted as not required

--- public/scripts/pins.js
+++
@@ -1,9 +0,0 @@
-// submitPinInfo is not currently being used - we might end up deleting this
-
-const submitPinInfo = function (data) {
-  return $.ajax({
-    method: "POST",
-    url: "/api/pins",
-    data,
-  });
-}
