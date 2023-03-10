ca0f930a73f67cbce87b6effb3b9a27cb27fe230
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-03-09 17:07:52 -0800

    Change what mapQueries.getMapById(mapID) renders

--- routes/maps.js
+++ routes/maps.js
@@ -41,7 +41,8 @@ router.get('/:id', (req, res) => {
   mapQueries.getMapById(mapID)
   .then(map => {
     const templateVars = { map, user: userID, key }
-    res.render('one_map', templateVars);
+    // res.render('one_map', templateVars);
+    res.render('index', templateVars);
   })
 })
 
