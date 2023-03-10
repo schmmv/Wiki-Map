673a7eec4703edd279d1aeb53c45e6a586f2a626
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-03-10 22:48:49 +0000

    Change img links in seed data

--- db/seeds/03_pins.sql
+++ db/seeds/03_pins.sql
@@ -5,6 +5,6 @@ VALUES (1, 1, 'UBC', 'Ranked top 20 worldwide', 'https://github.com/schmmv/Wiki-
 (1, 2, 'Miner''s Path', 'Easy 2km walk to a waterfall', 'https://github.com/schmmv/Wiki-Map/blob/master/public/images/miners_path.jpg?raw=true', 49.63871384026196, -114.50494142050628),
 (1, 2, 'Star Creek Falls', 'Nice short hike along the river ending in a great view of the falls', 'https://github.com/schmmv/Wiki-Map/blob/master/public/images/star_creek.jpg?raw=true', 49.6274044255323, -114.53859833158945),
 (2, 3, 'Greenbelt Trailhead', 'Easy 4 mile loop trail', 'https://github.com/schmmv/Wiki-Map/blob/master/public/images/greenbelt.jpg?raw=true', 50.995512050761604, -118.20266841360453),
-(2, 4, 'Royal Tyrrell Museum', 'Ultimate dinosaur museum', 'https://lh5.googleusercontent.com/p/AF1QipNza5_QxoDsL9iMedYDXBcygkmr58-YTBzpJBAf=w408-h306-k-no', 51.479685173715005, -112.78995105600498),
+(2, 4, 'Royal Tyrrell Museum', 'Ultimate dinosaur museum', 'https://github.com/schmmv/Wiki-Map/blob/master/public/images/tyrell.jpg?raw=true', 51.479685173715005, -112.78995105600498),
 (1, 5, 'Fernie Brewing Company', 'Best beer in town', 'https://github.com/schmmv/Wiki-Map/blob/master/public/images/fernie_brewing.jpg?raw=true', 49.52681206007952, -115.0476444279735),
-(2, 5, 'Oldman River Brewing', 'The one and only - don''t miss the Ephemerella hopped sour!', 'https://lh5.googleusercontent.com/p/AF1QipPiXjtVgDJOVWanYS0dtu9PK8uTGGDMFydU74fb=w408-h408-k-no',49.58717747698649, -114.1623618865719);
+(2, 5, 'Oldman River Brewing', 'The one and only - don''t miss the Ephemerella hopped sour!', 'https://github.com/schmmv/Wiki-Map/blob/master/public/images/old_man_river.jpg?raw=true',49.58717747698649, -114.1623618865719);
